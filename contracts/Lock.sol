e4173a679737ebbbabf2ad2c01d14718a05d5626
Andrews Nobre <andrewsnobre@gmail.com>
2023-05-25 13:53:31 -0300

    fim

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -294,27 +294,6 @@ contract Lock is ERC20 {
         }
     }
 
-    /*   function verifysign(
-        bytes32 _hashedMessage,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
-    ) public view returns (bool) {
-        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        bytes32 prefixedHashMessage = keccak256(
-            abi.encodePacked(prefix, _hashedMessage)
-        );
-        address signer = ecrecover(prefixedHashMessage, _v, _r, _s);
-
-        // if the signature is signed by the owner
-        if (signer == owner) {
-            // give player (msg.sender) a prize
-            return true;
-        }
-
-        return false;
-    } */
-
     function signers(
         bytes32 _ethSignedMessageHash,
         bytes memory _signature,
